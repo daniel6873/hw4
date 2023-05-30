1dd2f0e5063721dcd3a1bd2f28cc641a886f8b04
Daniel Mobayyen <daniel.mobayyen@kellogg.northwestern.edu>
2023-05-30 01:46:19 +0000

    05/29 Commit 1_v1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,10 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by({ "email" => params["email"] })
     if @user
-      if BCrypt::Password.new(@user["password"]) == BCrypt::Password.params["password"]
+      if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
         flash["notice"] = "Welcome, #{@user["username"]}."
-        redirect_to "/companies"
+        redirect_to "/places"
       else
         flash["notice"] = "Nope."
         redirect_to "/login"
