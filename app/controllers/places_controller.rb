c1fc0d9e9255a5b96dc6032e1d8184ff82987937
Daniel Mobayyen <daniel.mobayyen@kellogg.northwestern.edu>
2023-05-30 23:57:46 +0000

    commit 2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,7 +5,7 @@ class PlacesController < ApplicationController
   end
 
   def show
-    @place = Place.find_by({ "id" => params["id"] })
+    @place = Place.find_by({ "id" => params["id"] }) & { "user_id" => @current_user })
     @posts = Post.where({ "place_id" => @place["id"] } & { "user_id" => @current_user })
   end
 
